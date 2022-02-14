2728fd750f7b8fb9aab439b9fc27ad1b11a6685a
hekatonkheirex <hekatonkheirex@gmail.com>
2022-02-13 22:41:33 -0300

    Updated config

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -19,7 +19,7 @@
 --vim.g.tokyonight_italic_functions = true
 --vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 --vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.g.rose_pine_variant = 'main'  -- 'main', 'moon', 'dawn'
+vim.g.rose_pine_variant = 'dawn'  -- 'main', 'moon', 'dawn'
 --vim.g.rose_pine_bold_vertical_split_line = false
 --vim.g.rose_pine_inactive_background = false
 --vim.g.rose_pine_disable_background = false
