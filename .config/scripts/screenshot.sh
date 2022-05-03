6db2144a0f4c3143272fd808945b0a42477398a3
Rodrigo Murayama <hekatonkheirex@gmail.com>
2022-05-02 22:12:21 -0400

    Font changes to Material Design Icons to Font Awesome

--- .config/scripts/screenshot.sh
+++ .config/scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-filename="/home/mura/Pictures/Sreenshots/$(date +'%Y.%m.%d-%H:%m:%S').png"
+filename="/home/mura/Pictures/Screenshots/$(date +'%Y.%m.%d-%H:%m:%S').png"
 touch $filename
 grim $filename
